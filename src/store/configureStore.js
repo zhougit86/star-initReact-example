808b61b640679e8675a3c853ec26272b6508484a
zhougit86 <zhou16386@163.com>
2017-05-30 21:36:18 +0800

    add log midware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,14 +5,22 @@
 
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
+import createLogger from 'redux-logger'
 
 import rootReducer from './reducer';
+
+
+
+const middleware = [ thunk ];
+if ( process.env.NODE_ENV !== 'development' ) {
+    middleware.push(createLogger());
+}
 
 export default function configureStore(initialState) {
 
     const store = createStore(rootReducer, initialState, compose(
         applyMiddleware(
-            thunk
+            ...middleware
         ),
         // Add other middleware on this line...
         window.devToolsExtension ? window.devToolsExtension() : f => f // add support for Redux dev tools
